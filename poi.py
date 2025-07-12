d8f8b70543a9591847458646725961a7b1e8a8e8
m2k1609 <m2k1609@gmail.com>
2025-07-12 11:06:13 -0400

    school class

--- poi.py
+++ poi.py
@@ -52,39 +52,151 @@
 
 
 
-class account:
-    bank_name = "bank of america"
+# class account:
+#     bank_name = "bank of america"
     
     
-    def __init__(self,name,number,type,balance):
-        self.name=name
-        self.number=number
-        self.type=type
-        self.balance=balance
+#     def __init__(self,name,number,type,balance):
+#         self.name=name
+#         self.number=number
+#         self.type=type
+#         self.balance=balance
 
-    def typeofaccounts(self):
-        y = input("what type of account do you have")
-        return f"You have a {y} account"
+#     def typeofaccounts(self):
+#         y = input("what type of account do you have")
+#         return f"You have a {y} account"
     
-    def numbers(self):
-        z = int(input("what is your account number?"))
-        return f"We are now accessing {z} account"
+#     def numbers(self):
+#         z = int(input("what is your account number?"))
+#         return f"We are now accessing {z} account"
     
-    def withdraw(self):
-        a = int(input("How much would you like to withdraw")) 
-        if a>self.balance:
-            print("Your broke bruh")
+#     def withdraw(self):
+#         a = int(input("How much would you like to withdraw")) 
+#         if a>self.balance:
+#             print("Your broke bruh")
+#         else:
+#             return f"we are withdrawing ${a} from your account"
+
+#     def deposit(self):
+#         b = int(input("How much would you like to deposit"))
+#         return f"we have deposited ${b} into your account"
+    
+# moneythingy = account("bank of america",123,"checking",100)
+
+# print(moneythingy.bank_name)
+# print(moneythingy.withdraw())
+# print(moneythingy.deposit())
+# print(moneythingy.numbers())
+# print(moneythingy.typeofaccounts())
+
+
+
+
+
+
+
+class School:
+    school_name = "howtoberich"
+    
+    def __init__(self, name, number, type, students):
+        self.name = name
+        self.number = number
+        self.type = type
+        self.students = students
+
+    def get_students(self):
+      
+        return f"This school has {self.students} number of students"
+    
+    def get_school_type(self):
+        
+        return f"This is a {self.type} school"
+    
+    def get_school_number(self):
+        a = int(input("What is the school number? "))
+        return f"We are now accessing school number {a}"
+    
+    def add_student(self):
+        b = int(input("How many students would you like to add? "))
+        self.students=b + self.students
+        if b <= 0:
+            print("You can't add zero or negative students!")
         else:
-            return f"we are withdrawing ${a} from your account"
-
-    def deposit(self):
-        b = int(input("How much would you like to deposit"))
-        return f"we have deposited ${b} into your account"
+            return f"We are adding {b} students to the school"
     
-moneythingy = account("bank of america",123,"checking",100)
+    def remove_student(self):
+        c = int(input("How many students are leaving? "))
+        self.students=self.students - c
+        if c > self.students:
+            print("You can't remove more students than you have!")
+        else:
+            return f"We are removing {c} students from the school"
 
-print(moneythingy.bank_name)
-print(moneythingy.withdraw())
-print(moneythingy.deposit())
-print(moneythingy.numbers())
-print(moneythingy.typeofaccounts())
+# Create a school instance
+my_school = School("howtoberich", 123, "high school", 5000)
+
+print(my_school.school_name)
+print(my_school.get_students())
+print(my_school.add_student())
+print(my_school.get_students())
+print(my_school.remove_student())
+print(my_school.get_students())
+
+print(my_school.get_school_type())
+print(my_school.get_school_number())
+
+
+
+# class Student:
+#     school_name = "Purdue University"
+    
+#     def __init__(self, name, student_id, major, grade):
+#         self.name = name
+#         self.student_id = student_id
+#         self.major = major
+#         self.grade = grade
+    
+#     def get_major(self):
+        
+#         return f"You are studying {self.major}"
+    
+#     def get_student_id(self):
+      
+#         return f"We are now accessing student ID {self.student_id}"
+    
+#     def add_grade(self):
+#         grade = int(input("What grade did you receive? "))
+#         self.grade = self.grade + grade
+#         if self.grade < 0 or self.grade > 100:
+#             print("Invalid grade! Grade must be between 0 and 100")
+#         else:
+#             return f"Adding grade {grade} to your record"
+    
+#     def check_grade(self):
+        
+#         if self.grade < 60:
+#             print("You're failing, study harder!")
+#         else:
+#             return f"Your current grade is {self.grade}"
+    
+#     def enroll_course(self):
+#         course = input("What course would you like to enroll in? ")
+#         return f"You have enrolled in {course}"
+
+
+# student1 = Student("Mohammad Khan", 12345, "Computer Science", 0)
+
+# print(student1.school_name)
+# print(student1.get_major())
+# print(student1.get_student_id())
+# print(student1.add_grade())
+# print(student1.check_grade())
+# print(student1.enroll_course())
+
+
+
+
+
+
+
+# class gpa please help
